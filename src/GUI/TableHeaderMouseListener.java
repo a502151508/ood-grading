344a61ed83be1e08ff24651fff690a2b7857ac60
yifeifeng <ghoulknight95@gmail.com>
2019-04-28 16:38:31 -0400

    finish lock view

--- src/GUI/TableHeaderMouseListener.java
+++ src/GUI/TableHeaderMouseListener.java
@@ -3,16 +3,24 @@ package GUI;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import entity.Grade;
+import entity.dto.StudentGradingDto;
+
 public class TableHeaderMouseListener extends MouseAdapter {
     private JTable table;
-    
-    public TableHeaderMouseListener(JTable table) {
+    private List<StudentGradingDto> rowDataList;
+    private List<Integer> subTaskIdList;
+    public TableHeaderMouseListener(JTable table, List<StudentGradingDto> rowDataList,List<Integer> subTaskIdList) {
         this.table = table;
+        this.rowDataList = rowDataList;
+        this.subTaskIdList = subTaskIdList;
     }
      
     public void mouseClicked(MouseEvent event) {
@@ -21,9 +29,36 @@ public class TableHeaderMouseListener extends MouseAdapter {
          
         JOptionPane.showMessageDialog(table, "Column header #" + column + " is clicked");
         DefaultTableModel currentTableModel = (DefaultTableModel) table.getModel();
-        currentTableModel.addRow(new Object[]{"test1","10","9","8"});
+        DefaultTableModel newTableModel = new DefaultTableModel();
+        newTableModel.addColumn("Name");
+        //  currentTableModel.addRow(new Object[]{"test1","10","9","8"});
         
+        newTableModel.addColumn(currentTableModel.getColumnName(column));
+        int modifiedSubtaskId = subTaskIdList.get(column);
         //table.setColumnSelectionInterval(column,column); 
+       
+        //set row
+       
+        for(StudentGradingDto sgdto : this.rowDataList) {
+        	List<String> data = new ArrayList<>();
+    		String name = sgdto.getFirstName() + " " + sgdto.getLastName();
+    		data.add(name);
+    		
+    		List<Grade> gradeList = sgdto.getGrades();
+    		for(Grade g : gradeList) {
+    			int subTaskId = g.getSubTaskId();
+    			if(modifiedSubtaskId == subTaskId) {
+    				data.add(Double.toString(g.getScore()));
+    			}
+    			
+    		}
+    	//	this.rowDataList.add(data);
+    		String[] dataString = data.toArray(new String[0]);
+    		newTableModel.addRow(dataString);
+        }
+        
+        table.setModel(newTableModel);
+        
         // do your real thing here...
     }
 }
--- src/GUI/gradeView.java
+++ src/GUI/gradeView.java
@@ -5,6 +5,17 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 
+import com.sun.prism.paint.Gradient;
+
+import entity.Grade;
+import entity.SubTask;
+import entity.dto.StudentGradingDto;
+import entity.dto.TaskDto;
+import service.GradeService;
+import service.TaskService;
+import service.impl.GradeServiceImpl;
+import service.impl.TaskServiceImpl;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -12,15 +23,27 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class gradeView extends JFrame {
     private JTable gradeTable;
+    private TaskService ts = new TaskServiceImpl();
+    private GradeService gs = new GradeServiceImpl();
+    private List<String> columns ;
+    private List<Integer> subTaskIdList;
+    private List<StudentGradingDto> rowDataList;
+    private HashMap<Integer,Integer> subTaskPositionMap = new HashMap<>();
     public static void main(String[] args){
         gradeView frame = new gradeView();
      //   frame.setVisible(true);
 
     }
     public gradeView(){
+    	columns  = new ArrayList<>();
+    	subTaskIdList = new ArrayList<>();
+    	subTaskIdList.add(0, -1);
+    	rowDataList = new ArrayList<>();
     	this.setLayout(new BorderLayout());
     	Container contentPane = this.getContentPane();
         JPanel buttonPanel = createButtonPanel();
@@ -56,7 +79,7 @@ public class gradeView extends JFrame {
         scrollPane.setViewportView(gradeTable);
        
         JTableHeader header = gradeTable.getTableHeader();
-        header.addMouseListener(new TableHeaderMouseListener(gradeTable));
+        header.addMouseListener(new TableHeaderMouseListener(gradeTable,this.rowDataList,this.subTaskIdList));
         
         
         
@@ -66,14 +89,54 @@ public class gradeView extends JFrame {
     protected void setTableContent()
     {
     	DefaultTableModel tableModel = new DefaultTableModel();
-    	ArrayList<String> column = new ArrayList<String>(Arrays.asList("Name","hw1","hw2","midterm1"));
-    	for(String c : column) {
-    		tableModel.addColumn(c);
+    	//ArrayList<String> column = new ArrayList<String>(Arrays.asList("Name","hw1","hw2","midterm1"));
+    	//set column
+    	List<TaskDto> l =ts.getTaskList(2);
+    	
+    	tableModel.addColumn("Name");
+    	
+    	for(TaskDto t : l) {
+    		List<SubTask> subTaskList = t.getSubTaskList();
+    		int i = 0;
+    		for(SubTask subtask : subTaskList) {
+    			//columns.add(subtask.getSubTaskName());
+    			i++;
+    			tableModel.addColumn(subtask.getSubTaskName());
+    			//need hashmap to memory position
+    			subTaskIdList.add(i,subtask.getSubTaskId());
+    			subTaskPositionMap.put(subtask.getSubTaskId(), i);
+    			
+    		}
+    		
+    		
     	}
+    	
+    	//set data
+    	List<StudentGradingDto> studentGradingList = gs.getGradingList(2);
+    	for(StudentGradingDto sgdto : studentGradingList) {
+    		this.rowDataList.add(sgdto);
+    		List<String> data = new ArrayList<>();
+    		String name = sgdto.getFirstName() + " " + sgdto.getLastName();
+    		data.add(name);
+    		
+    		List<Grade> gradeList = sgdto.getGrades();
+    		for(Grade g : gradeList) {
+    			int subTaskId = g.getSubTaskId();
+    			data.add(subTaskPositionMap.get(subTaskId), Double.toString(g.getScore()));
+    		}
+    	//	this.rowDataList.add(data);
+    		String[] dataString = data.toArray(new String[0]);
+    		tableModel.addRow(dataString);
+    	}
+    	
+    	
     //	ArrayList<String> row = new ArrayList<String>(Arrays.asList("YIFEI","100","90","80"));
     	
-    	 tableModel.addRow(new Object[]{"YIFEI","100","90","80"});
+    	// tableModel.addRow(new Object[]{"YIFEI","100","90","80"});
         gradeTable.setRowHeight(30);
         gradeTable.setModel(tableModel);
     }
-}
+    private List<StudentGradingDto> getRowData(){
+    		return this.rowDataList;
+    	}
+  	}
